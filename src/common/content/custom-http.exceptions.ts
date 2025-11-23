97117e830a048239c76a0cc00ef49145e073bf4a
orprogrammer <orprogrammer@gmail.com>
2025-11-23 14:17:18 +0330

    Add custom HTTP exceptions for enhanced error handling, including NotFound, Forbidden, RequestTimeout, Unauthorized, BadRequest, and CustomThrottler exceptions.

---
+++ src/common/content/custom-http.exceptions.ts
@@ -0,0 +1,42 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export class CustomHttpException extends HttpException {
+  constructor(message: string, statusCode: number) {
+    super(message, statusCode);
+  }
+}
+
+export class NotFoundException extends CustomHttpException {
+  constructor(message: string) {
+    super(message, HttpStatus.NOT_FOUND);
+  }
+}
+
+export class ForbiddenException extends CustomHttpException {
+  constructor(message: string) {
+    super(message, HttpStatus.FORBIDDEN);
+  }
+}
+
+export class RequestTimeoutException extends CustomHttpException {
+  constructor(message: string) {
+    super(message, HttpStatus.REQUEST_TIMEOUT);
+  }
+}
+
+export class UnauthorizedException extends CustomHttpException {
+  constructor(message: string) {
+    super(message, HttpStatus.UNAUTHORIZED);
+  }
+}
+
+export class BadRequestException extends CustomHttpException {
+  constructor(message: string) {
+    super(message, HttpStatus.FORBIDDEN);
+  }
+}
+export class CustomThrottlerException extends CustomHttpException {
+  constructor(message: string) {
+    super(message, HttpStatus.TOO_MANY_REQUESTS);
+  }
+}
